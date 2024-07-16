8910f74447b64b5c6e40d93db3dc916be0f47bc1
Damien Gallagher <damien.gallagher@gmail.com>
2024-07-16 14:41:28 +0100

    Add loading skeleton

--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import { Container, Theme } from '@radix-ui/themes';
+import AuthProvider from './auth/Provider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,15 +26,17 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <Theme appearance="light" accentColor="mint" grayColor="mauve">
-          <NavBar />
-          <main className='p-5'>
-            <Container>
-              {children}
-            </Container>
-          </main>
-          {/* <ThemePanel /> */}
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="mint" grayColor="mauve">
+            <NavBar />
+            <main className='p-5'>
+              <Container>
+                {children}
+              </Container>
+            </main>
+            {/* <ThemePanel /> */}
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )
