7a32272f1361d2713425fef13badfc0bcd429e99
Damien Gallagher <damien.gallagher@gmail.com>
2024-07-16 15:30:22 +0100

    Fetch data with react query

--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import { Container, Theme } from '@radix-ui/themes';
 import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -26,18 +27,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <AuthProvider>
-          <Theme appearance="light" accentColor="mint" grayColor="mauve">
-            <NavBar />
-            <main className='p-5'>
-              <Container>
-                {children}
-              </Container>
-            </main>
-            {/* <ThemePanel /> */}
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme appearance="light" accentColor="mint" grayColor="mauve">
+              <NavBar />
+              <main className='p-5'>
+                <Container>
+                  {children}
+                </Container>
+              </main>
+              {/* <ThemePanel /> */}
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
-    </html>
+    </html >
   )
 }
