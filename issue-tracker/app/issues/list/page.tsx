7ace6f88632d4a130ac0df881d302196773953a9
Damien Gallagher <damien.gallagher@gmail.com>
2024-07-17 10:54:34 +0100

    Refactor issue table

--- issue-tracker/app/issues/list/page.tsx
+++ issue-tracker/app/issues/list/page.tsx
@@ -1,30 +1,14 @@
-import prisma from '@/prisma/client'
-import { Table } from '@radix-ui/themes'
-import { Link, IssueStatusBadge } from '@/app/components';
-import IssueActions from './IssueActions'
-import { Issue, Status } from '@prisma/client';
-import NextLink from 'next/link';
-import { ArrowUpIcon } from '@radix-ui/react-icons';
+import prisma from '@/prisma/client';
+import { Status } from '@prisma/client';
+import IssueActions from './IssueActions';
 import Pagination from '@/app/components/Pagination';
+import IssueTable, { columnNames, IssueQuery } from './IssueTable';
+import { Flex } from '@radix-ui/themes';
 
 interface Props {
-  searchParams: {
-    status: Status,
-    orderBy: keyof Issue,
-    page: string
-  }
+  searchParams: IssueQuery
 }
 const IssuesPage = async ({ searchParams }: Props) => {
-
-  const columns: {
-    label: string;
-    value: keyof Issue;
-    className?: string;
-  }[] = [
-      { label: 'Issue', value: 'title' },
-      { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
-      { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' }
-    ]
 
   const statuses = Object.values(Status);
 
@@ -32,7 +16,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined
 
-  const orderBy = columns.map(column => column.value)
+  const orderBy = columnNames
     .includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: 'asc' }
     : undefined;
@@ -41,9 +25,9 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const pageSize = 10;
 
   const where = {
-      status
-    };
-    
+    status
+  };
+
   const issues = await prisma.issue.findMany({
     where,
     orderBy,
@@ -55,42 +39,11 @@ const IssuesPage = async ({ searchParams }: Props) => {
     where
   })
   return (
-    <div>
+    <Flex direction='column' gap='3'>
       <IssueActions />
-      <Table.Root variant='surface'>
-        <Table.Header>
-          <Table.Row>
-            {columns.map(column =>
-              <Table.ColumnHeaderCell key={column.value} className={column.className}>
-                <NextLink href={{
-                  query: { ...searchParams, orderBy: column.value }
-                }}>
-                  {column.label}
-                </NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className='inline' />}
-              </Table.ColumnHeaderCell>)}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map(issue => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
-
-                <div className='block md:hidden'>
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-          <Pagination pageSize={pageSize} currentPage={page} itemCount={issueCount}/>
-    </div>
+      <IssueTable searchParams={searchParams} issues={issues} />
+      <Pagination pageSize={pageSize} currentPage={page} itemCount={issueCount} />
+    </Flex>
   )
 }
 
--- issue-tracker/app/page.tsx
+++ issue-tracker/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from 'next/image'
-import Pagination from './components/Pagination'
-
-export default function Home({searchParams}: {searchParams:{page: string}}) {
+export default function Home() {
   return (
     <div>
       Hello World
-      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
     </div>
   )
 }
